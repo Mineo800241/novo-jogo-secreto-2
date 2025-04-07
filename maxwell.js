d61a482c68e16eccd12921aaa13b66833c40581d
Mineo800241 <33529992+Mineo800241@users.noreply.github.com>
2025-04-06 22:26:53 -0300

    ultimamexida

--- maxwell.js
+++ maxwell.js
@@ -1,4 +1,5 @@
 //Olá estou tentando o git commit
 //mas apenas funciona o git add.
 // quer ver? 
-// verificar que só funciona no terminal do git 
+// verificar aqui que só funciona no terminal do git 
+// fazendo um novo teste
